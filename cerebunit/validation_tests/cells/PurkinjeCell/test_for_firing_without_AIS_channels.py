2c03ae5ffde7cdadca4baa0bb2af36e0110d3855
Lungsi <lungsi.scholarpedia@gmail.com>
2017-10-02 17:30:01 +0200

    Initial Commit

--- cerebunit/validation_tests/cells/PurkinjeCell/test_for_firing_without_AIS_channels.py
+++ cerebunit/validation_tests/cells/PurkinjeCell/test_for_firing_without_AIS_channels.py
@@ -4,6 +4,22 @@
 # created  01 September 2017 Lungsi
 # modified 29 September 2017 Lungsi
 #
+# Use case:
+# o first import the test
+#   from cerebunit.validation_tests.cells.PurkinjeCell
+#               import NoChannelsAISTest as noAIStest
+# o experimental_data is the loaded json data wrapped
+#   with python quantities. This is handled by the
+#   HBP validation framework.
+# o instantiate the noAIStest with experimental_data
+#   test = noAIStest(experimental_data)
+# o run the test on desired model
+#   s = test.judge(desired_model, deep_error=True)
+# o then you can see the outputs to
+#   s
+#   s.score
+#   s.description
+#
 # ============================================================================
 
 import sciunit
@@ -24,7 +40,11 @@ class NoChannelsAISTest(sciunit.Test, BinaryScore):
 
     def generate_prediction(self, model, verbose=False):
         '''
-        fdf
+        Generates spike train from "vm_soma", cell region.
+        The function is automatically called by sciunit.Test
+        which this test is a child of.
+        Therefore as part of sciunit generate_prediction is
+        mandatory.
         '''
         setup_parameters = { "dt": 0.025,   "celsius": 37,
                              "tstop": 1000, "v_init": -65 }
@@ -35,21 +55,26 @@ class NoChannelsAISTest(sciunit.Test, BinaryScore):
 
     def process_prediction(self, model):
         '''
-        afdd
+        Once the model has run, this function can be used to
+        process the spike_train prediction to get the
+        prediction of interest, mean firing rate.
         '''
         cell_region = "vm_soma"
-        return mfr(model.predictions["spike_train"][cell_region])
+        x = mfr(model.predictions["spike_train"][cell_region])
+        return x.rescale(pq.Hz)
 
-
-    def validate_observation(self, observation, first_try=True):
-        '''
-        fdfd
-        '''
-        pass
 
     def compute_score(self, observation, model, verbose=False):
         '''
-        fdfd
+        This is function like generate_prediction is called
+        automatically by sciunit which this test is a child of.
+        This function with the same name compute_score is also
+        therefore mandatory.
+        This function calls the function process_prediction to
+        return the mean firing rate of spike train from vm_soma.
+        This is then compared against the experimental_data to
+        get the binary score; 0 if the prediction correspond
+        with experiment, otherwise 1.
         '''
         processed_prediction = self.process_prediction(model)
         a_prediction = processed_prediction.item() # just the magnitude
