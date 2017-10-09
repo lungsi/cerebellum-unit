a526df08c687ce17efeb732f25d30a6b2515556e
Lungsi <lungsi.scholarpedia@gmail.com>
2017-10-09 11:40:45 +0200

    Initial Commit

--- cerebunit/score_manager.py
+++ cerebunit/score_manager.py
@@ -74,6 +74,55 @@ class BinaryScore(sciunit.Score):
         return "BinaryScore is " + str(self.score)
 # ============================================================================
 #
+# =========================BinaryMatrixScore==================================
+# created  21 September 2017 Lungsi
+# modified 29 September 2017 Lungsi
+#
+class BinaryMatrixScore(sciunit.Score):
+    '''
+    A Binary Score.
+    0 if the prediction is not in the interval of the measurement within a margin of error (epsilon).
+    '''
+    #
+    # -----------------------------Use Case-----------------------------------
+    # x = BinaryScore.compute( measurement, prediction )
+    # score = BinaryScore(x)
+    # ------------------------------------------------------------------------
+    #
+    @classmethod
+    def compute(self, list_of_binary_scores, breakdown_of_binary_scores):
+        # mesurement is in dictionary form whose value has
+        # magnitude and python quantity
+        # default epsilon = 10**(-3)
+        no_of_scores = len(list_of_binary_scores)
+        cummulative_score = sum(list_of_binary_scores)
+        if cummulative_score == 0:
+            self.score = 0
+        elif (cummulative_score >= 0) and (cumulative_score < no_of_scores):
+            self.score = 0
+        elif cummulative_score == no_of_scores:
+            self.score = 1
+        #
+        self.score_breakdown = breakdown_of_binary_scores
+        return self.score
+
+    _description = ( "The BinaryScore gives a score of 0 or 1 based on the comparison between prediction vs. measurement. "
+                   + "The prediction is a python quantities, i.e, it is in the form of array(x.x) * <some_unit>. "
+                   + "The measurement is also a python quantity, but in dictionary form. "
+                   + "If the measurement has only 1-key, its value is the one that is the reference. "
+                   + "The predicted value is then compared against the measurement with margin of error given by a default epsilon value. "
+                   + "Therefore, BinaryScore checks that the predicted value is inside the interval. "
+                   + "If it is not it, a score-0 is given. "
+                   + "If the measurement has an addition error-key its value is the epsilon.")
+
+    @property
+    def sort_key(self):
+        return self.score
+
+    def __str__(self):
+        return "BinaryMatrixScore is " + str(self.score)
+# ============================================================================
+#
 # ==========================Score=============================================
 class NameHereScore(sciunit.Score):
     '''
